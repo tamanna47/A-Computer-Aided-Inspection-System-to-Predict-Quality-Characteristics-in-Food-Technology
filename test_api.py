7d9085617feed06b4600fa3c295dbaf27d6d726d
tamanna47 <tamannarafeeq26@gmail.com>
2025-12-12 21:45:31 +0530

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,19 @@
+import requests
+
+# Sensor input data
+data = {
+    "sensor_2": 0.0023,
+    "sensor_3": -0.0004,
+    "sensor_4": 100.0,
+    "sensor_7": 520.5,
+    "sensor_8": 639.9,
+    "sensor_9": 1583.0,
+    "sensor_11": 21.8,
+    "sensor_12": 40.1,
+    "sensor_13": 39.0,
+    "sensor_14": 84.5,
+    "sensor_15": 520.1,
+    "sensor_17": 392.0,
+    "sensor_20": 39.9,
+    "sensor_21": 23.8
+}
